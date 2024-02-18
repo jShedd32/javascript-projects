05eb21451860b11d94d7c2a1679d0dc909b64e74
jShedd32 <shedd32@hotmail.com>
2024-02-18 17:00:17 -0600

    data-variables-conditionals studio done

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -1,8 +1,8 @@
 // Initialize Variables below
-let date = Monday 2019-03-18
-let time = 10:05:34 AM
+let date = "Monday 2019-03-18"
+let time = "10:05:34 AM"
 let astronautCount = 7
-let astronautStatus = ready
+let astronautStatus = "ready"
 let averageAstronautMassKg = 80.7
 let crewMassKg = astronautCount * averageAstronautMassKg
 let fuelMassKg = 760000
@@ -12,18 +12,35 @@ let maximumMassLimit = 850000
 let fuelTempCelsius = -225
 let minimumFuelTemp = -300
 let maximumFuelTemp = -150
-let weatherStatus = clear 
+let fuelLevel = 100
+let weatherStatus = "clear" 
 let preparedForLiftOff = true
+
+
 // add logic below to verify total number of astronauts for shuttle launch does not exceed 7
-
+if (astronautCount <= 7){
+    console.log("Astronaut Count: " + astronautCount);
+}
 // add logic below to verify all astronauts are ready
-
+if (astronautStatus === "ready"){
+    console.log("Astronaut Status: " + astronautStatus);
+}
 // add logic below to verify the total mass does not exceed the maximum limit of 850000
-
+if (totalMassKg <= 850000){
+    console.log("Total Mass: " + totalMassKg);
+}
 // add logic below to verify the fuel temperature is within the appropriate range of -150 and -300
-
+if (fuelTempCelsius >= -300 && fuelTempCelsius <= -150){
+    console.log("Fuel Temp: " + fuelTempCelsius);
+}
 // add logic below to verify the fuel level is at 100%
+if (fuelLevel == 100){
+    console.log("Fuel Level: "+ fuelLevel +"%");
+}
 
 // add logic below to verify the weather status is clear
+if (weatherStatus == "clear"){
+    console.log("Weather Status: "+ weatherStatus);
 
+}
 // Verify shuttle launch can proceed based on above conditions
